bb316437e78bc155967e42f12d41a3d4f0db02b9
JohnPaulZigterman <johnpaulzigterman@gmail.com>
2023-10-07 15:25:07 -0400

    added more prompt functionality and cleaned up errant code

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ inquirer
         {
             type: 'input',
             message: 'Please provide installation instructions.',
-            name: 'linkedin',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'Please describe how to use that application.',
+            name: 'usage',
         },
         {
             type: 'input',
@@ -28,10 +33,56 @@ inquirer
             message: 'Please provide testing instructions.',
             name: 'testing',
         },
+        {
+            type: 'input',
+            message: 'Please provide installation instructions.',
+            name: 'installation',
+        },
+        {
+            type: 'list',
+            message: 'Please select a license.',
+            choices: ['MIT', 'Open Source'],
+            name: 'license',
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'github',
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email',
+        },
     ])
     .then((response) =>
         fs.writeFile(response.title.toLowerCase().split(" ").join("") + ".md", `
-        
+        # ${response.title}
+
+        ## Table of Contents
+
+        1. Project Description
+        2. Installation
+        3. Contribution Guidelines
+        4. Test Instructions
+
+        ## Project Description
+
+        ${response.description}
+
+        ## Installation
+
+        ${response.installation}
+
+        ## Contribution Guidelines
+
+        ${response.contributions}
+
+        ## Test Instructions
+
+        ${response.testing}
+
+        When creating an open source project on
         `, (err) =>
             err ? console.error(err) : console.log('success!')
         )
